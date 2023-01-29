1b3a74a8187ca0971a970976717f6348ea68a780
Hoger Shoresh <sarahadi@Saras-Air.broadband>
2023-01-29 18:49:48 +0000

    added functions to the js code

--- script.js
+++ script.js
@@ -1,17 +1,36 @@
-var currentDay = moment();
-$('#currentDay').text(currentDay.format('dddd, DD MMMM YYYY'));
-
-var startTime = moment("9:00", "HH:mm");
-var endTime = moment("18:00", "HH:mm");
-
-
 $(function() {
-    for (var time = startTime; time.isBefore(endTime); time.add(1, 'hours')) {
-      var row = $("<div>").addClass("row d-flex justify-content-center");
-      $("<h1>").addClass("hour").text(time.format("HH:mm")).appendTo(row);
-      $("<textarea>").addClass("description").appendTo(row);
-      $("<button>").addClass("saveBtn").html("<i class='fa fa-save'></i>").appendTo(row);
-      row.appendTo(".container");
-    }
+  var currentDay = moment().format('dddd, DD MMMM YYYY');
+  $('#currentDay').text(currentDay);
+  
+  var startTime = moment("9:00", "HH:mm");
+  var endTime = moment("18:00", "HH:mm");
+  var currentTime = moment().format("HH:00");
+  
+  for (var time = startTime; time.isBefore(endTime); time.add(1, 'hours')) {
+  var hour = time.format("HH:00");
+  $("<div>").addClass("row d-flex justify-content-center")
+  .append($("<h1>").addClass("hour").text(hour))
+  .append($("<textarea>").addClass("description " + (currentTime === hour ? 'present' : (currentTime > hour ? 'past' : 'future'))))
+  .append($("<button>").addClass("saveBtn").html("<i class='fa fa-save'></i>"))
+  .appendTo(".container");
+  }
+  
+  $(".saveBtn").click(function(){
+  var description = $(this).prev(".description");
+  var i = $(".description").index(description);
+  var entryData = "entry" + i;
+  localStorage.setItem(entryData, description.val());
+  alert("Saved!")
   });
   
+  $(".description").each(function(i) {
+  var entryData = "entry" + i;
+  var stored = localStorage.getItem(entryData);
+  $(this).val(stored || "");
+  });
+  });
+
+
+
+
+
