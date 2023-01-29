e13f90bfb0765786f2fb82549c88b089b83be66f
Hoger Shoresh <sarahadi@Saras-Air.broadband>
2023-01-29 19:31:31 +0000

    resized the textarea to make it more wider

--- script.js
+++ script.js
@@ -5,9 +5,10 @@ $(function() {
   var startTime = moment("9:00", "HH:mm");
   var endTime = moment("18:00", "HH:mm");
   var currentTime = moment().format("HH:00");
+  console.log(currentTime);
   
   for (var time = startTime; time.isBefore(endTime); time.add(1, 'hours')) {
-  var hour = time.format("HH:00");
+  var hour = time.format("HH:00"); 
   $("<div>").addClass("row d-flex justify-content-center")
   .append($("<h1>").addClass("hour").text(hour))
   .append($("<textarea>").addClass("description " + (currentTime === hour ? 'present' : (currentTime > hour ? 'past' : 'future'))))
@@ -18,13 +19,13 @@ $(function() {
   $(".saveBtn").click(function(){
   var description = $(this).prev(".description");
   var i = $(".description").index(description);
-  var entryData = "entry" + i;
+  var entryData = "scheduleEntry" + i;
   localStorage.setItem(entryData, description.val());
   alert("Saved!")
   });
   
   $(".description").each(function(i) {
-  var entryData = "entry" + i;
+  var entryData = "scheduleEntry" + i;
   var stored = localStorage.getItem(entryData);
   $(this).val(stored || "");
   });
