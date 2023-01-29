14cc6a4d1808ff8e7941191ee56880c91a7cfc64
Hoger Shoresh <hogga1@gmail.com>
2023-01-29 23:48:25 +0000

    change margin and padding values of the webpage to make it all fit without scrolling

---
+++ script.js
@@ -0,0 +1,43 @@
+$(function() {
+  var currentDay = moment().format('dddd, Do MMMM YYYY');
+  $('#currentDay').text(currentDay);
+  var previousDay = localStorage.getItem("previousDay");
+  if (previousDay !== currentDay) {
+    localStorage.clear();
+  }
+  localStorage.setItem("previousDay", currentDay);
+  
+  var startTime = moment("9:00", "HH:mm");
+  var endTime = moment("18:00", "HH:mm");
+  var currentTime = moment().format("HH:00");
+  
+  for (var time = startTime; time.isBefore(endTime); time.add(1, 'hours')) {
+  var hour = time.format("HH:00"); 
+  $("<div>").addClass("row d-flex justify-content-center")
+  .append($("<h1>").addClass("hour").text(hour))
+  .append($("<textarea>").addClass("description " + (currentTime === hour ? 'present' : (currentTime > hour ? 'past' : 'future'))))
+  .append($("<button>").addClass("saveBtn").html("<i class='fa fa-save'></i>"))
+  .appendTo(".container");
+  }
+  
+  $(".saveBtn").click(function(){
+  $("#modal").modal('show');
+  var description = $(this).prev(".description");
+  var i = $(".description").index(description);
+  var entryData = "scheduleEntry" + i;
+  localStorage.setItem(entryData, description.val());
+  });
+  
+  $(".description").each(function(i) {
+  var entryData = "scheduleEntry" + i;
+  var stored = localStorage.getItem(entryData);
+  $(this).val(stored || "");
+  });
+  });
+
+
+
+
+
+
+
