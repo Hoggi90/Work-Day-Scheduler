6d827c7b39301bb2e3faf57a97a012cb28e2a444
Hoger Shoresh <hogga1@gmail.com>
2023-01-31 20:03:48 +0000

    edited layout of js script

--- assets/script.js
+++ assets/script.js
@@ -3,14 +3,20 @@
 $(function() {
   var currentDay = moment().format('dddd, DD MMMM YYYY');
   $('#currentDay').text(currentDay);
+
+  
 // This will check what day it is, and clear localstorage if it's next day
   localStorage.setItem("previousDay", currentDay);
   var previousDay = localStorage.getItem("previousDay");
-  if (previousDay !== currentDay) {localStorage.clear();}  
+  if (previousDay !== currentDay) {localStorage.clear();};
+
+
 // These are some variables used to construct the time-block
   var startTime = moment("9:00", "HH:mm");
   var endTime = moment("18:00", "HH:mm");
   var currentTime = moment().format("HH:00");
+
+
 // This for loop, creates each hourly time block for every loop
   for (var time = startTime; time.isBefore(endTime); time.add(1, 'hours')) {
   var hour = time.format("HH:00"); 
@@ -19,7 +25,9 @@ $(function() {
   .append($("<textarea>").addClass("description " + (currentTime === hour ? 'present' : (currentTime > hour ? 'past' : 'future'))))
   .append($("<button>").addClass("saveBtn").html("<i class='fa fa-save'></i>"))
   .append($("<button>").addClass("clearBtn").html("<i class='fa fa-trash'></i>"))
-  .appendTo(".container");}
+  .appendTo(".container");};
+
+
 // This nested function will save the entry to localstorage upon clicking the button
   $(".saveBtn").click(function(){
   $("#modal").modal('show');
@@ -28,6 +36,9 @@ $(function() {
   var entryData = "scheduleEntry" + i;
   localStorage.setItem(entryData, description.val());
 });
+
+
+
 // This nested function clears the textarea and saved the entry to localstorage
 $(".clearBtn").click(function(){
   $("#clearModal").modal('show');
@@ -37,6 +48,7 @@ $(".clearBtn").click(function(){
   description.val("");
   localStorage.removeItem(entryData);
 });
+
 
 
 // This nested function retrieves the entry from localstorage
